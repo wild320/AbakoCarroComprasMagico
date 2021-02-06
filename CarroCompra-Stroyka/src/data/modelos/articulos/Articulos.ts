5ad66b09f9a62cf6c7f09fdeccc42f2fcb449ca8
alejandroev <46693503+alejandroev@users.noreply.github.com>
2021-02-06 08:38:45 -0500

    Update Articulos.ts
    2021-02-06 AEV

--- CarroCompra-Stroyka/src/data/modelos/articulos/Articulos.ts
+++ CarroCompra-Stroyka/src/data/modelos/articulos/Articulos.ts
@@ -13,5 +13,29 @@ export class ArticulosCarroComprasResponse {
         label: string;
         url: string;
     }>;
+    products: {
+        page: number;
+        limit: number;
+        sort: string;
+        total: number;
+        pages: number;
+        from: number;
+        to: number;
+        filters: Array<{
+            max: number;
+            min: number;
+            name: string;
+            slug: string;
+            type: string;
+            value: number[];
+            items: Array <{
+                id: number;
+                name: string;
+                slug: string;
+                type: string;
+                count: number;
+            }>;
+        }>;
+    };
 
 }
