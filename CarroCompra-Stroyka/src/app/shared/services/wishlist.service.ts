74facf4dbd9b0e156b03d425da2c49ac0ef800b5
dito <info@magicomundo.co>
2025-02-06 09:42:01 -0500

    feat: Ordenar favoritos por sku, y gris sin inventario

--- CarroCompra-Stroyka/src/app/shared/services/wishlist.service.ts
+++ CarroCompra-Stroyka/src/app/shared/services/wishlist.service.ts
@@ -1,15 +1,15 @@
 import { isPlatformBrowser } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable, OnDestroy, PLATFORM_ID } from '@angular/core';
-import { IndividualConfig, ToastrService } from 'ngx-toastr';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
+import { BehaviorSubject, firstValueFrom, Observable, of, Subject, throwError } from 'rxjs';
+import { catchError, takeUntil, tap } from 'rxjs/operators';
 import { CServicios } from '../../../data/contantes/cServicios';
+import { Item } from '../../../data/modelos/articulos/Items';
 import { ServiceHelper } from './ServiceHelper';
 import { LocalService } from './local-service.service';
 import { NegocioService } from './negocio.service';
 import { UsuarioService } from './usuario.service';
-import { Item } from '../../../data/modelos/articulos/Items';
 
 interface WishlistData {
     items: Item[];
@@ -19,57 +19,44 @@ interface WishlistData {
     providedIn: 'root'
 })
 export class WishlistService implements OnDestroy {
-    private data: WishlistData = {
-        items: []
-    };
-
+    private data: WishlistData = { items: [] };
     private destroy$: Subject<void> = new Subject();
     private itemsSubject$: BehaviorSubject<Item[]> = new BehaviorSubject<Item[]>([]);
     private onAddingSubject$: Subject<Item> = new Subject();
     private UrlServicioFavoritos: string;
-    private UrlServicio: string;
     private dataUserLoged: any;
     private token = 'token';
-    private itemsFavoritos: any = [];
+    private itemsFavoritos: Item[] = [];
 
     readonly items$: Observable<Item[]> = this.itemsSubject$.asObservable().pipe(takeUntil(this.destroy$));
     readonly onAdding$: Observable<Item> = this.onAddingSubject$.asObservable();
-    private quantitySubject$: BehaviorSubject<number> = new BehaviorSubject(this.itemsFavoritos.length);
+    private quantitySubject$: BehaviorSubject<number> = new BehaviorSubject<number>(this.itemsFavoritos.length);
     readonly count$: Observable<number> = this.quantitySubject$.asObservable();
 
-
-
-
     constructor(
-        @Inject(PLATFORM_ID)
-        private platformId: Object,
+        @Inject(PLATFORM_ID) private platformId: Object,
         private toastr: ToastrService,
-        public usuariosvc: UsuarioService,
+        private usuarioService: UsuarioService,
         private localService: LocalService,
-        private negocio: NegocioService,
+        private negocioService: NegocioService,
         private httpClient: HttpClient,
-        private servicehelper: ServiceHelper<any, any>,
+        private serviceHelper: ServiceHelper<any, any>
     ) {
         if (isPlatformBrowser(this.platformId)) {
             this.load();
         }
     }
 
-
-
-    async CargarUsuario() {
+    private async loadUserData() {
         if (isPlatformBrowser(this.platformId)) {
-            const usr = this.localService.getJsonValue(this.token) ?? this.localService.getJsonValueSession(this.token);
-            this.dataUserLoged = JSON.parse(usr) ?? null;
+            const user = this.localService.getJsonValue(this.token) ?? this.localService.getJsonValueSession(this.token);
+            this.dataUserLoged = JSON.parse(user) ?? null;
         }
     }
 
-
-
     async add(product: Item) {
-
         if (!this.dataUserLoged) {
-            this.toastr.error('Para agregar un producto a lista de deseos debe iniciar sesión');
+            this.toastr.error('Para agregar un producto a la lista de deseos debe iniciar sesión');
             return;
         }
 
@@ -79,10 +66,10 @@ export class WishlistService implements OnDestroy {
             dllFavorito: [{ idArticulo: product.id }]
         };
 
-        this.UrlServicioFavoritos = `${this.negocio.configuracion.UrlServicioCarroCompras}${CServicios.ApiCarroCompras}${CServicios.ServicioFavoritos}`;
+        this.UrlServicioFavoritos = `${this.negocioService.configuracion.UrlServicioCarroCompras}${CServicios.ApiCarroCompras}${CServicios.ServicioFavoritos}`;
 
         try {
-            const result = await this.servicehelper.PostData(this.UrlServicioFavoritos, productRequest).toPromise();
+            const result = await this.serviceHelper.PostData(this.UrlServicioFavoritos, productRequest).toPromise();
 
             if (result.mensaje.msgId === 1) {
                 this.onAddingSubject$.next(product);
@@ -91,90 +78,90 @@ export class WishlistService implements OnDestroy {
                 this.toastr.error('El producto no fue agregado a la lista de favoritos');
             }
         } catch (err) {
-            console.error('Error adding product to wishlist:', err);
+            console.error('Error al agregar producto a la lista de deseos:', err);
             this.toastr.error('Ocurrió un error al intentar agregar el producto a la lista de favoritos');
         }
     }
 
-
-    remove(product: Item) {
+    remove(product: Item): Observable<any> {
         const index = this.itemsFavoritos.findIndex(item => item.id === product.id);
-        this.itemsFavoritos.splice(index, 1)
-        this.itemsSubject$.next(this.itemsFavoritos);
-        localStorage.setItem("favoritos", JSON.stringify(this.itemsFavoritos))
-        this.quantitySubject$.next(this.itemsFavoritos.length);
-        const productrequest = {
-            "proceso": "DEL",
-            "IdPersona": parseInt(this.dataUserLoged.IdEmp),
-            "dllFavorito": [
-                {
-                    "idArticulo": product.id
-                }
-            ]
+        if (index === -1) {
+            console.warn('Producto no encontrado en la lista de favoritos');
+            return of(null);
         }
-        //  this.onAddingSubject$.next(product);
-        this.toastr.error(`El producto fue eliminado correctamente`);
-        this.UrlServicioFavoritos = this.negocio.configuracion.UrlServicioCarroCompras + CServicios.ApiCarroCompras + CServicios.ServicioFavoritos;
-        return this.httpClient.post(this.UrlServicioFavoritos, productrequest)
+
+        this.itemsFavoritos.splice(index, 1);
+        this.itemsFavoritos.sort((a, b) => a.sku.localeCompare(b.sku));
+        this.itemsSubject$.next(this.itemsFavoritos);
+        localStorage.setItem("favoritos", JSON.stringify(this.itemsFavoritos));
+        this.quantitySubject$.next(this.itemsFavoritos.length);
+
+        const productRequest = {
+            proceso: "DEL",
+            IdPersona: parseInt(this.dataUserLoged?.IdEmp ?? '0'),
+            dllFavorito: [{ idArticulo: product.id }]
+        };
+
+        this.toastr.success('El producto fue eliminado correctamente');
+
+        this.UrlServicioFavoritos = `${this.negocioService.configuracion.UrlServicioCarroCompras}${CServicios.ApiCarroCompras}${CServicios.ServicioFavoritos}`;
+
+        return this.httpClient.post(this.UrlServicioFavoritos, productRequest).pipe(
+            tap(() => console.log("Producto eliminado del servidor correctamente")),
+            catchError(error => {
+                console.error("Error al eliminar producto del servidor:", error);
+                return throwError(() => error);
+            })
+        );
     }
-
-
 
     private async load() {
         try {
-            // Cargar usuario y manejar si no está logueado
-            this.CargarUsuario();
+            await this.loadUserData();
+
             if (!this.dataUserLoged) {
-                this.toastr.error('Para agregar un producto a lista de deseos debe iniciar sesión');
-                return; // Si no hay usuario logueado, salir de la función
+                this.toastr.error('Para agregar un producto a la lista de deseos debe iniciar sesión');
+                return;
             }
-    
-            // Preparar request para obtener los favoritos
+
             const productRequest = {
                 proceso: 'GET',
                 IdPersona: parseInt(this.dataUserLoged?.IdEmp ?? '0'),
                 dllFavorito: [{ idArticulo: 0 }]
             };
-    
-            // Construir URL del servicio de favoritos
-            this.UrlServicioFavoritos = `${this.negocio.configuracion.UrlServicioCarroCompras}${CServicios.ApiCarroCompras}${CServicios.ServicioFavoritos}`;
-            
-            // Realizar petición para obtener los productos favoritos
-            const result = await this.servicehelper.PostData(this.UrlServicioFavoritos, productRequest).toPromise();
-    
-            // Iterar sobre los productos favoritos y obtener detalles
+
+            this.UrlServicioFavoritos = `${this.negocioService.configuracion.UrlServicioCarroCompras}${CServicios.ApiCarroCompras}${CServicios.ServicioFavoritos}`;
+
+            const result = await firstValueFrom(this.serviceHelper.PostData(this.UrlServicioFavoritos, productRequest));
+
             const favoritosPromises = result.favoritos.map(async (element: any) => {
-                // URL para obtener detalles del artículo
-                const articuloUrl = `${this.negocio.configuracion.UrlServicioCarroCompras}${CServicios.ApiCarroCompras}${CServicios.ServicioRecuperarArticulosDetalle}/${this.dataUserLoged.IdEmp}/${element.idArticulo}`;
-                
+                const articuloUrl = `${this.negocioService.configuracion.UrlServicioCarroCompras}${CServicios.ApiCarroCompras}${CServicios.ServicioRecuperarArticulosDetalle}/${this.dataUserLoged.IdEmp}/${element.idArticulo}`;
+
                 try {
-                    const config: any = await this.httpClient.get(articuloUrl).toPromise();
-                    
-                    // Verificar si el artículo ya está en favoritos
+                    const config: any = await firstValueFrom(this.httpClient.get(articuloUrl));
+
                     if (!this.itemsFavoritos.some(item => item.id === config.articulo.id)) {
                         this.itemsFavoritos.push(config.articulo);
-                        this.quantitySubject$.next(this.itemsFavoritos.length);
-                        localStorage.setItem('favoritos', JSON.stringify(this.itemsFavoritos));
                     }
                 } catch (error) {
                     console.error('Error al obtener detalle del artículo:', error);
                 }
             });
-    
-            // Esperar a que se resuelvan todas las promesas
+
             await Promise.all(favoritosPromises);
-    
-            // Emitir los nuevos favoritos
+
+            // Ordenar los favoritos por SKU solo una vez después de cargarlos todos
+            this.itemsFavoritos.sort((a, b) => (a.sku ?? '').localeCompare(b.sku ?? ''));
+
+            // Actualizar almacenamiento local y emitir cambios
+            localStorage.setItem('favoritos', JSON.stringify(this.itemsFavoritos));
             this.itemsSubject$.next(this.itemsFavoritos);
-    
+            this.quantitySubject$.next(this.itemsFavoritos.length);
+
         } catch (err) {
-            // Manejo de errores generales
             console.error('Error al cargar favoritos:', err);
         }
     }
-    
-
-
 
     ngOnDestroy(): void {
         this.destroy$.next();
